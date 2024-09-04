ecb93f155d30479b02de0a6eef6001fdc5544c01
Natalia Krasnykh <krasnih.n161@mail.ru>
2024-09-04 12:06:13 +0300

    Bonus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int price = 35067;
+        int mile = 20;
+        int bonus;
 
+        bonus = price / mile;
+        System.out.println("При покупке билета стоимостью " + price + " начисляются бонусные мили в количестве " + bonus + " шт");
     }
 }
